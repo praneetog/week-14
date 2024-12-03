4a440d92f036c856a09247c5b0d301caf5537dde
praneet <praneetkashyap001@gmail.com>
2024-12-03 10:28:41 +0530

    basic fullstack next

--- next-backend/app/page.tsx
+++ next-backend/app/page.tsx
@@ -1,25 +1,26 @@
 import axios from "axios";
 
-async function getUserDetails() {
-  await new Promise((res) => setTimeout(res, 1000));
-  const response = await axios.get("http://localhost:3000/api/user")
-	return response.data;
-}
+// async function getUserDetails() {
+//   await new Promise((res) => setTimeout(res, 1000));
+//   const response = await axios.get("http://localhost:3000/api/user")
+// 	return response.data;
+// }
 
 export default async function Home() {
-  const userData = await getUserDetails();
+  // const userData = await getUserDetails();
 
   return (
-    <div className="flex flex-col justify-center h-screen">
-        <div className="flex justify-center">
-            <div className="border p-8 rounded">
-                <div>
-                    Name: {userData?.name}
-                </div>
+    <div></div>
+    // <div className="flex flex-col justify-center h-screen">
+    //     <div className="flex justify-center">
+    //         <div className="border p-8 rounded">
+    //             <div>
+    //                 Name: {userData?.name}
+    //             </div>
                 
-                {userData?.email}
-            </div>
-        </div>
-    </div>
+    //             {userData?.email}
+    //         </div>
+    //     </div>
+    // </div>
   );
 }
