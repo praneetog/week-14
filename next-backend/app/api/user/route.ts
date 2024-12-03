0fd3bf185dc2b5475114db5689526dc8e589760e
praneet <praneetkashyap001@gmail.com>
2024-12-03 18:02:56 +0530

    better fetches

--- next-backend/app/api/user/route.ts
+++ next-backend/app/api/user/route.ts
@@ -1,20 +1,28 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 const client = new PrismaClient();
 
 //Backend in Next.js
 export async function POST(req: NextRequest) {
     const body = await req.json();
-    client.user.create({
-        data: {
-            username: body.username,
-            password: body.password
-        }
-    })
-
-    return Response.json({
-        message: "You are logged in!"
-    })
+    try{
+        await client.user.create({
+            data: {
+                username: body.username,
+                password: body.password
+            }
+        })
+    
+        return Response.json({
+            body
+        })
+    } catch(e) {
+        return NextResponse.json({
+            message: "Error while signing up"
+        }, {
+            status: 411
+        })
+    }
 }
 
 
